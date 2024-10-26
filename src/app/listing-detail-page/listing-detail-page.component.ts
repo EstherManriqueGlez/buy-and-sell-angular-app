f4ad8f03b2cbb267e1254e9a08b05dd0769f8a55
Esther Manrique <esthermanriqueepam@gmail.com>
2024-10-26 10:19:40 -0600

    Added styles and linting-detail-page

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './listing-detail-page.component.html',
-  styleUrl: './listing-detail-page.component.scss'
+  styleUrl: './listing-detail-page.component.scss',
 })
-export class ListingDetailPageComponent {
+export class ListingDetailPageComponent implements OnInit {
+  listing: Listing | undefined;
 
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find((listing) => listing.id === id);
+  }
 }
--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
+import { NgFor } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listings-page',
   standalone: true,
-  imports: [],
+  imports: [NgFor, RouterLink],
   templateUrl: './listings-page.component.html',
-  styleUrl: './listings-page.component.scss'
+  styleUrl: './listings-page.component.scss',
 })
-export class ListingsPageComponent {
+export class ListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
 
+
+  ngOnInit(): void {
+    this.listings = fakeListings;
+  }
 }
