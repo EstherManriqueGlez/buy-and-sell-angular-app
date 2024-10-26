7fe99d6f6554f5ed84451b210c7d4af69dfd8404
Esther Manrique <esthermanriqueepam@gmail.com>
2024-10-26 14:31:54 -0600

    Added Listing data form component and new listing page changes.

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -7,7 +7,7 @@ import {FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports:  [FormsModule,],
+  imports:  [FormsModule],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
