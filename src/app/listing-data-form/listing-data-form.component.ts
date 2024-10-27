d284d3f3b2a13fc2aa8a1b480c65098f7b813a59
Esther Manrique <esthermanriqueepam@gmail.com>
2024-10-26 19:43:18 -0600

    Fixed issues in listing data form component

--- src/app/listing-data-form/listing-data-form.component.ts
+++ src/app/listing-data-form/listing-data-form.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listing-data-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './listing-data-form.component.html',
   styleUrl: './listing-data-form.component.scss'
 })
-export class ListingDataFormComponent {
+export class ListingDataFormComponent implements OnInit{
+@Input() buttonText: any;
+
+  name: string = '';
+  description: string = '';
+  price: string = '';
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    alert('Creating a new listing...');
+    this.router.navigateByUrl('/my-listings');
+  }
 
 }
