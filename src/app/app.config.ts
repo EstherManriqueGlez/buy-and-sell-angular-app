cecbaf66c5117d8b6dfd2adff6860c937ed059f6
Esther Manrique <esthermanriqueepam@gmail.com>
2024-12-20 18:04:08 -0600

    Changed proxi.config.json file

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HttpClient, HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,7 +11,5 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(),
     provideHttpClient(),
-    HttpClient,
-    HttpClientModule,
   ],
 };
